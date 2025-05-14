e1fcb6a807650b46592b4fb9efa1f635caa4ac67
Dinh Giap Than <thankt26k4@gmail.com>
2025-05-14 13:14:38 +0700

    bai1

---
+++ src/main/java/com/example/bai1/Bai1Servlet.java
@@ -0,0 +1,15 @@
+package com.example.bai1;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class Bai1Servlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Dinh Giap Than</h1>");
+    }
+}
